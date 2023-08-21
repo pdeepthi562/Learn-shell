f3cd31ffb23c016d0cfe43cd58e9753a7be708aa
dpathim <dpdevops6@gmail.com>
2023-08-21 16:28:54 -0400

    Functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,7 +1,11 @@
 greeting() {
   echo Hello,Goodmorning
   echo welcome
+  Return 10
   echo Good to have you hear.
 }
 
 greeting
+echo Function Exit Status - $?
+
+#Functions also as exit status that by using Return.
