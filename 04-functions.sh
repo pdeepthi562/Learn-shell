8af971ce99fbb29c320bf97f8c410722120c7a61
dpathim <dpdevops6@gmail.com>
2023-08-21 16:40:17 -0400

    Functions

--- 04-functions.sh
+++ 04-functions.sh
@@ -8,4 +8,14 @@ greeting() {
 greeting
 echo Function Exit Status - $?
 
-#Functions also as exit status that by using Return.
+#Functions also as exit status that by using return.
+# You can declare var in main program, you can access that in function and Vice Versa.
+
+input() {
+  echo First Argument - $1
+  echo Second Argument -$2
+  echo All Arguments -$*
+  echo No of Argument -$#
+}
+
+input abc 123
